1a3f9beefa18f3e5ea16efd013d4bb3e986d93e6
ionut Vaida <ionut.vaida@gmail.com>
2020-12-07 20:54:49 +0200

    Create settings

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1,11 +1,69 @@
 #include <Settings.h>
+#include <ArduinoJson.h>
 
-void Settings::ReadSettings()
+Settings::Settings()
 {
-
+    CreateConfigFileIfNotExist();
+    ReadSettings();
 }
 
-void Settings::SaveSettings()
+bool Settings::ReadSettings()
 {
-    
+    File tmpFile = SPIFFS.open("/Settings.json", FILE_READ);
+    String content = tmpFile.readString();
+
+    DynamicJsonDocument settingsJson(512);
+
+    deserializeJson(settingsJson, content);
+    JsonObject settingsObj = settingsJson.as<JsonObject>();
+
+    JsonObject wifiObj = settingsObj["WIFI"].as<JsonObject>();
+    m_ssid = wifiObj["SSID"].as<String>();
+    m_password = wifiObj["Password"].as<String>();
+
+    JsonObject homeObj = settingsObj["HOME"].as<JsonObject>();
+    m_hightTemp = homeObj["HighTemp"];
+    m_lowTemp = homeObj["LowTemp"];
+    tmpFile.close();
+
+    return true;
+}
+
+bool Settings::SaveSettings(bool t_override) const
+{
+    if (t_override)
+    {
+        SPIFFS.remove("/Settings.json");
+    }
+    File tmpFile = SPIFFS.open("/Settings.json", FILE_WRITE);
+    DynamicJsonDocument settingsJson(512);
+
+    DynamicJsonDocument wifi(200);
+    wifi["SSID"] = m_ssid;
+    wifi["Password"] = m_password;
+    settingsJson["WIFI"] = wifi;
+
+    DynamicJsonDocument home(200);
+    home["HighTemp"] = m_hightTemp;
+    home["LowTemp"] = m_lowTemp;
+    settingsJson["HOME"] = home;
+
+    serializeJson(settingsJson, tmpFile);
+    tmpFile.close();
+
+    return true;
+}
+
+void Settings::CreateConfigFileIfNotExist()
+{
+    if (!SPIFFS.begin(true))
+    {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+    if (!SPIFFS.exists("/Settings.json"))
+    {
+        Serial.println("Settings file not found");
+        SaveSettings(true);
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 #include <BLEDevice.h>
 #include <BluetoothReader.h>
 #include <ClientCallback.h>
-#include <ArduinoJson.h>
-#include "SPIFFS.h"
+#include <Settings.h>
+
 
 #define LYWSD03MMC_ADDR "A4:C1:38:0A:B3:DB"
 
 BluetoothReader *m_reader = nullptr;
+Settings *m_settings = nullptr;
 
 void setup()
 {
@@ -18,30 +19,11 @@ void setup()
   BLEDevice::init("ESP32");
   m_reader = new BluetoothReader(BLEUUID("ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6"), LYWSD03MMC_ADDR);
   m_reader->SetCallback(new ClientCallback());
-  if (!SPIFFS.begin(true))
-  {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
+  m_settings = new Settings();
 }
 
 void loop()
 {
-  Serial.println("read file");
-  File root = SPIFFS.open("/");
-  File file = root.openNextFile();
-  while(file){
- 
-      Serial.print("FILE: ");
-      Serial.println(file.name());
- 
-      file = root.openNextFile();
-}
-  File test = SPIFFS.open("/test.txt");
-  //DynamicJsonBuffer doc;
-  String file_content = test.readString();
-  //const char* caca = doc["HOME"];
-  Serial.println(file_content);
   if (!m_reader->IsConnected())
   {
     try
